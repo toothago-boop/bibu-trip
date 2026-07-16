5f78c9789ef3665e1688c1ebf965fed0b183f973
toothago-boop <toothago@gmail.com>
2026-07-16 13:03:38 +0800

    Add files via upload

--- public/firebase-config.js
+++ public/firebase-config.js
@@ -2,10 +2,11 @@
 // Paste the values from your bibu-1d00a web app below. This config is not a secret;
 // Firestore Security Rules protect the data.
 export const firebaseConfig={
-  apiKey:'PASTE_FIREBASE_API_KEY',
+  apiKey:'AIzaSyCoAAEdflBXyip7XXF_xPsumE3W2neS2lc',
   authDomain:'bibu-1d00a.firebaseapp.com',
   projectId:'bibu-1d00a',
   storageBucket:'bibu-1d00a.firebasestorage.app',
-  messagingSenderId:'PASTE_MESSAGING_SENDER_ID',
-  appId:'PASTE_FIREBASE_APP_ID'
+  messagingSenderId:'282284560648',
+  appId:'1:282284560648:web:f23764e87b1a1872d6dcbd',
+  measurementId:'G-DSS9YR7XF5'
 };
